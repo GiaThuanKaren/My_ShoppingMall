b9cefcc4216bebc4ff85f454b5cc5b459c10da88
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-06-30 12:46:40 +0700

    Add Material Library 30/6/2022

--- client/clientui/src/App.js
+++ client/clientui/src/App.js
@@ -1,21 +1,11 @@
 import logo from "./logo.svg";
 import "../src/index.css";
-import Item from "./Components/Item/item";
 import { io } from "socket.io-client";
 function User() {
   const arr = Array.from(Array(10).keys());
   return (
-    <div className="w-full block p-0 ">
-      <h2 className="font-bold text-xl">All Pizzas</h2>
-      <div className="flex flex-wrap h-screen">
-        {arr.map((item, id) => {
-          return (
-            <>
-              <Item data={item} />
-            </>
-          );
-        })}
-      </div>
+    <div className="">
+      
     </div>
   );
 }
