581c3c1a88d9b88a21b1d96dad21888d7ffa0ff9
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-08-28 15:38:54 +0700

    Add Container Component To Wrap All Products 29/8/2022

--- client/clientui/src/page/Home/Home.tsx
+++ client/clientui/src/page/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, TextField, Typography } from "@mui/material";
+import { Button, Container, Grid, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import clsx from "clsx";
 import React, { useEffect, useId, useState } from "react";
@@ -28,53 +28,62 @@ function Home() {
   return (
     <>
       <HomeSlider />
-      {state.DataList1.length > 0 &&
-        state.DataList1.map((item: any, idx: any) => {
-          return (
-            <>
-              <ListProduct
-                tittle={item.title}
-                Data={item.products}
-                isTittleCenter
-              />
-            </>
-          );
-        })}
-      <section className={clsx([style.Home_ContactSection])}>
-        <Grid container justifyContent={"space-between"} alignItems={"center"}>
-          <Grid item>
-            <Typography fontWeight={"500"} component={"p"}>
-              Đăng ký để nhận ưu đãi
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <TextField
-                id="outlined-basic"
-                label="Outlined Email"
-                variant="outlined"
-              />
-              <Button sx={{height:"100%"}} variant="contained">Contained</Button>
-            </Box>
-          </Grid>
-          <Grid item>
-            <Grid container spacing={1}>
-              <Grid item>
-                <FontAwesomeIcon icon={IconBrand.faInstagram} />
-              </Grid>
-              <Grid item>
-                <FontAwesomeIcon icon={IconBrand.faFacebook} />
-              </Grid>
-              <Grid item>
-                <FontAwesomeIcon icon={IconBrand.faTwitter} />
-              </Grid>
-              <Grid item>
-                <FontAwesomeIcon icon={IconBrand.faPinterest} />
+      <Container>
+        {state.DataList1.length > 0 &&
+          state.DataList1.map((item: any, idx: any) => {
+            return (
+              <>
+                <ListProduct
+                  tittle={item.title}
+                  Data={item.products}
+                  isTittleCenter
+                />
+              </>
+            );
+          })}
+
+        <section className={clsx([style.Home_ContactSection])}>
+          <Grid
+            container
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <Grid item>
+              <Typography fontWeight={"500"} component={"p"}>
+                Đăng ký để nhận ưu đãi
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <TextField
+                  id="outlined-basic"
+                  label="Outlined Email"
+                  variant="outlined"
+                />
+                <Button sx={{ height: "100%" }} variant="contained">
+                  Contained
+                </Button>
+              </Box>
+            </Grid>
+            <Grid item>
+              <Grid container spacing={1}>
+                <Grid item>
+                  <FontAwesomeIcon icon={IconBrand.faInstagram} />
+                </Grid>
+                <Grid item>
+                  <FontAwesomeIcon icon={IconBrand.faFacebook} />
+                </Grid>
+                <Grid item>
+                  <FontAwesomeIcon icon={IconBrand.faTwitter} />
+                </Grid>
+                <Grid item>
+                  <FontAwesomeIcon icon={IconBrand.faPinterest} />
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </section>
+        </section>
+      </Container>
     </>
   );
 }
